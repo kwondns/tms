9c04d0912ac0ba064bcffd58a23bf4575ae0f85d
Hanjun Kwon <kwondns@gmail.com>
2024-02-24 14:11:37 +0900

    🚀 Feat: App Module
    - ConfigModule 을 사용하여 환경변수 활용
    - Dev, Test 환경에서의 DB 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: `.env.${process.env.NODE_ENV}.local` }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'dev.sqlite',
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
     }),
+    AdminModule,
   ],
   controllers: [AppController],
   providers: [AppService],
