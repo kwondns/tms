a1753da5e679aefb67e0b5d008c2469d3cf11ea5
Hanjun Kwon <kwondns@gmail.com>
2024-02-24 13:45:55 +0900

    🚀 Feat: Serialize Interceptor
    - 응답으로 반환되는 속성의 제어
    - DTO 를 인자로 받아 해당 DTO 에 @Expose 설정된 속성만 반환하도록 변환

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): object;
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<ClassConstructor>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
