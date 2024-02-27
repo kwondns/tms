c60ccf076c2c2e14df4ee5bfc5e04e8a1832ee52
Hanjun Kwon <kwondns@gmail.com>
2024-02-27 17:19:19 +0900

    🚀 Feat: Project
    - 수정, 생성은 인증
    - 단순 읽기는 퍼블릭으로 설정
    - Serialize 를 통한 반환 값 정의
    - Entity 에 cascade, ManyToOne 등 설정
    - 위 설정으로 Project 에 save 하면 관련 테이블 저장

---
+++ src/port/project/project.service.ts
@@ -0,0 +1,61 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from '../entities/project.entity';
+import { Repository } from 'typeorm';
+import { ProjectDetail } from '../entities/projectDetail.entity';
+import { ProjectDto } from '../dtos/project.dto';
+import { BackTag, FrontTag } from '../entities/projectTag.entity';
+
+@Injectable()
+export class ProjectService {
+  constructor(
+    @InjectRepository(Project) private projectRepo: Repository<Project>,
+    @InjectRepository(ProjectDetail) private projectDetailRepo: Repository<ProjectDetail>,
+    @InjectRepository(FrontTag) private frontTagRepo: Repository<FrontTag>,
+    @InjectRepository(BackTag) private backTagRepo: Repository<BackTag>,
+  ) {}
+
+  async getProjectAll() {
+    const result = await this.projectRepo.find({
+      where: { visible: true },
+      order: { created_at: 'asc' },
+      relations: ['front_tag', 'back_tag'],
+    });
+    if (result.length === 0) throw new BadRequestException('잘못된 요청입니다.');
+    return result;
+  }
+
+  async getProjectById(id: string) {
+    const result = await this.projectRepo.findOne({ where: { id }, relations: ['projectDetail'] });
+    if (!result) throw new BadRequestException('잘못된 요청입니다.');
+    return result.projectDetail.content;
+  }
+
+  async updateProject(id: string, projectAttrs: ProjectDto) {
+    const project = await this.projectRepo.findOne({
+      where: { id },
+      relations: ['projectDetail', 'back_tag', 'front_tag'],
+    });
+    if (!project.projectDetail) throw new BadRequestException('잘못된 요청입니다.');
+    const { content, front_tag, back_tag, ...others } = projectAttrs;
+    const front = front_tag.map((tag) => this.frontTagRepo.create({ front_tag: tag }));
+    const back = back_tag.map((tag) => this.backTagRepo.create({ back_tag: tag }));
+    project.projectDetail.content = content;
+    project.front_tag = front;
+    project.back_tag = back;
+    Object.assign(project, others);
+    return await this.projectRepo.save(project);
+  }
+
+  async createProject(projectAttrs: ProjectDto) {
+    const { content, front_tag, back_tag, ...others } = projectAttrs;
+    const projectDetail = this.projectDetailRepo.create({ content });
+    const front = front_tag.map((tag) => this.frontTagRepo.create({ front_tag: tag }));
+    const back = back_tag.map((tag) => this.backTagRepo.create({ back_tag: tag }));
+    const project = this.projectRepo.create(others);
+    project.projectDetail = projectDetail;
+    project.front_tag = front;
+    project.back_tag = back;
+    return this.projectRepo.save(project);
+  }
+}
