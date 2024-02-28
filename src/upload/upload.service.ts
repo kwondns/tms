d516cf41f8b9780a334f02a0e2abc17767bfbc94
Hanjun Kwon <kwondns@gmail.com>
2024-02-28 18:31:38 +0900

    🚀 Feat: Upload Service
    - 환경변수(ConfigService)를 사용하기 위하여 DI 추가
    - 파일 업로드 시 중간 경로에 현재 환경을 추가
    - dev, test, 배포 환경은 공란
    - 업로드 날짜도 중간에 디렉토리 생성

---
+++ src/upload/upload.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { randomBytes } from 'crypto';
+
+@Injectable()
+export class UploadService {
+  s3: S3Client;
+  bucket: string;
+  putPromises: any[];
+  putResults: string[];
+  constructor(private configService: ConfigService) {
+    this.s3 = new S3Client({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: this.configService.get<string>('S3_ACCESS'),
+        secretAccessKey: this.configService.get<string>('S3_SECRET'),
+      },
+    });
+    this.bucket = `${this.configService.get<string>('S3_BUCKET')}`;
+    this.putPromises = [];
+    this.putResults = [];
+  }
+  async uploadHandler(path: string, files: Array<Express.Multer.File>) {
+    const env = this.configService.get<string>('S3_ENV');
+    for (const file of files) {
+      await this.putS3(path, env, file);
+    }
+    await Promise.all(this.putPromises);
+    return this.putResults;
+  }
+
+  genRanHex() {
+    return randomBytes(8).toString('hex');
+  }
+
+  async putS3(path: string, env: string, file: Express.Multer.File) {
+    const fileExtension = file.originalname.split('.').pop();
+    const today = new Date().toLocaleDateString('ko-KR').slice(0, -1).replaceAll('.', '-').replaceAll(' ', '');
+    const filePath = `${env}${today}/${this.genRanHex()}.${fileExtension && fileExtension.toLowerCase()}`;
+    this.putResults.push(filePath);
+    const ContentDisposition = `attachment; filename*=UTF-8"${encodeURIComponent(file.originalname)}"`;
+    const command = new PutObjectCommand({
+      Bucket: `${this.bucket}-${path}`,
+      Key: filePath,
+      Body: file.buffer,
+      ACL: 'public-read',
+      ContentDisposition,
+    });
+    this.putPromises.push(this.s3.send(command));
+  }
+}
