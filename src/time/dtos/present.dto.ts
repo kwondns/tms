73678f4fe2cd48bf05ac9bbbb0fa5c36a1e89413
Hanjun Kwon <kwondns@gmail.com>
2024-03-09 20:43:45 +0900

    🚀 Feat: Present DTO 수정
    - title, content 도 옵셔널로 받을 수 있도록 수정
    - 데이터가 없는 경우에도 처리할 수 있도록 변경

--- src/time/dtos/present.dto.ts
+++ src/time/dtos/present.dto.ts
@@ -2,9 +2,11 @@ import { IsDateString, IsOptional, IsString } from 'class-validator';
 
 export class PresentDto {
   @IsString()
+  @IsOptional()
   title: string;
 
   @IsString()
+  @IsOptional()
   content: string;
 
   @IsDateString()
