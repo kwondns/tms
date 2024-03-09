aaf280856b295065f54a17bd5689fb379eff8bca
Hanjun Kwon <kwondns@gmail.com>
2024-03-09 23:29:40 +0900

    🚀 Socket 연결
    - `현재` 데이터가 업데이트 시 클라이언트에게 전송

--- src/time/present/present.service.ts
+++ src/time/present/present.service.ts
@@ -3,10 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Present } from '../entities/present.entity';
 import { Repository } from 'typeorm';
 import { PresentDto } from '../dtos/present.dto';
+import { PresentGateway } from '../events/present.gateway';
 
 @Injectable()
 export class PresentService {
-  constructor(@InjectRepository(Present) private presentRepo: Repository<Present>) {}
+  constructor(
+    @InjectRepository(Present) private presentRepo: Repository<Present>,
+    private presentGateway: PresentGateway,
+  ) {}
 
   async getPresent() {
     return this.presentRepo.findOneBy({ id: 1 });
@@ -15,6 +19,7 @@ export class PresentService {
   async storePresent(attrs: PresentDto) {
     const present = await this.getPresent();
     Object.assign(present, attrs);
-    return this.presentRepo.save(present);
+    this.presentGateway.presentUpdate(present);
+    return await this.presentRepo.save(present);
   }
 }
