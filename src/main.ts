2d5fcc7c6b5ce2835f4f22062ce7890e5ab7c075
Hanjun Kwon <kwondns@gmail.com>
2024-03-11 23:48:04 +0900

    🚀 Feat: Cors 설정
    - Production 환경에서는 내가 보유한 도메인에서만 접근할 수 있도록 설정
    - 그 외 환경에서는 localhost 에서 접근 하도록 변경

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,17 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({ origin: ['http://localhost:5173'], credentials: true });
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? (origin, callback) => {
+            if (origin && origin.endsWith('kwondns.site')) callback(null, true);
+            else callback(new Error('Not Allowed Origin!'), false);
+          }
+        : ['http://localhost:5173'],
+    methods: ['GET', 'PATCH', 'DELETE', 'POST', 'PUT'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
