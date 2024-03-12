a472ababf0e726769ec641ac6c04b563e9013107
Hanjun Kwon <kwondns@gmail.com>
2024-03-12 22:58:59 +0900

    🚑️ Fix: env 변수명 변경
    - 배포환경 변수명 변경후 적용

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,10 +1,10 @@
 export default () => {
   if (!process.env.NODE_ENV) {
     return {
-      JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-      JWT_REFRESH_EXPIRE: process.env.JWT_REFRESH_EXPIRE,
-      JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-      JWT_ACCESS_EXPIRE: process.env.JWT_ACCESS_EXPIRE,
+      TOKEN_REFRESH_SECRET: process.env.TOKEN_REFRESH_SECRET,
+      TOKEN_REFRESH_EXPIRE: process.env.TOKEN_REFRESH_EXPIRE,
+      TOKEN_ACCESS_SECRET: process.env.TOKEN_ACCESS_SECRET,
+      TOKEN_ACCESS_EXPIRE: process.env.TOKEN_ACCESS_EXPIRE,
       DB_NAME: process.env.DB_NAME,
       DB_PORT: process.env.DB_PORT,
       DB_USER_NAME: process.env.DB_USER_NAME,
