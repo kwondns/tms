ce90b57c2f7f886db19defc853e76e1935b3d722
kwondns <kwondns@gmail.com>
2025-06-02 15:05:43 +0900

    🔨 [Chore]: Loki 임시 제거

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,7 +1,7 @@
 import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import winston from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
-import LokiTransport from 'winston-loki';
+// import LokiTransport from 'winston-loki';
 import dotenv from 'dotenv';
 
 if (process.env.NODE_ENV === 'development') {
@@ -30,16 +30,16 @@ const commonFormat = (colors: boolean) =>
 
 export const winstonConfig = {
   transports: [
-    new LokiTransport({
-      host: `${process.env.LOKI_HOST}:${process.env.LOKI_PORT}`,
-      json: true, // JSON 형식 사용
-      batching: true, // 배치 처리 활성화
-      replaceTimestamp: true, // 타임스탬프 교체 (최신 버전에서는 기본값)
-      labels: { job: 'nestjs', app: 'tms', env: process.env.NODE_ENV },
-      format: commonFormat(false),
-      interval: 5, // 5초마다 로그 전송 (기본값)
-      onConnectionError: (err) => console.error('Loki 연결 실패:', err),
-    }),
+    // new LokiTransport({
+    //   host: `${process.env.LOKI_HOST}:${process.env.LOKI_PORT}`,
+    //   json: true, // JSON 형식 사용
+    //   batching: true, // 배치 처리 활성화
+    //   replaceTimestamp: true, // 타임스탬프 교체 (최신 버전에서는 기본값)
+    //   labels: { job: 'nestjs', app: 'tms', env: process.env.NODE_ENV },
+    //   format: commonFormat(false),
+    //   interval: 5, // 5초마다 로그 전송 (기본값)
+    //   onConnectionError: (err) => console.error('Loki 연결 실패:', err),
+    // }),
     new winston.transports.Console({
       level: 'info',
       format: commonFormat(true),
