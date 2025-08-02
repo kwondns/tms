7237924cbfe8b7699983e65ef1e4092b483e1187
kwondns <kwondns@gmail.com>
2025-08-02 11:45:56 +0900

    🚀 [Feat]: Future 및 FutureBox 서비스 트랜잭션 처리 로직 추가
    - `FutureService`의 메소드에 트랜잭션 처리 추가
      - `DataSource`를 활용하여 `QueryRunner` 생성 및 트랜잭션 관리
      - 예외 발생 시 트랜잭션 롤백, 정상 처리 시 커밋 수행
    - `findOneFutureBox` 메소드 추가로 FutureBox 단일 조회 기능 구현
    - `Future` 및 `FutureBox` 생성 및 업데이트 로직 개선
      - `Future`의 타입에 따라 적절한 저장소 사용 (`FutureCheck`, `FutureProgress`)
      - 불필요한 `priority` 파라미터 제거 및 일반화
    - 기타 리팩토링: `readonly` 키워드 추가, 속성 초기화 개선

--- src/time/future/future.service.ts
+++ src/time/future/future.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Future } from '@/time/entities/future.entity';
-import { Repository } from 'typeorm';
+import { Future, FutureCheck, FutureProgress } from '@/time/entities/future.entity';
+import { DataSource, Repository } from 'typeorm';
 import { FutureBox } from '@/time/entities/futureBox.entity';
 import { FutureDto } from '@/time/dtos/future.dto';
 import { FutureBoxDto } from '@/time/dtos/futureBox.dto';
@@ -11,46 +11,102 @@ import { FutureBoxCreateDto } from '@/time/dtos/futureBoxCreate.dto';
 @Injectable()
 export class FutureService {
   constructor(
-    @InjectRepository(Future) private futureRepo: Repository<Future>,
-    @InjectRepository(FutureBox) private futureBoxRepo: Repository<FutureBox>,
+    @InjectRepository(Future) private readonly futureRepo: Repository<Future>,
+    @InjectRepository(FutureBox) private readonly futureBoxRepo: Repository<FutureBox>,
+    private readonly dataSource: DataSource,
   ) {}
 
-  async getFutureBox(priority: number) {
+  async getFutureBox() {
     return this.futureBoxRepo.find({
-      where: { priority, checked: false },
+      where: { checked: false },
       order: { order: 'DESC' },
       relations: ['future'],
     });
   }
 
-  async getFutureBoxRecord(priority: number) {
-    return this.futureBoxRepo.find({ where: { priority, checked: true }, relations: ['future'] });
+  async getFutureBoxRecord() {
+    return this.futureBoxRepo.find({ relations: ['future'] });
+  }
+
+  async findOneFutureBox(id: string) {
+    return this.futureBoxRepo.findOneBy({ id });
   }
 
   async patchFuture(attrs: FutureDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     const future = await this.futureRepo.findOneBy({ id: attrs.id });
     if (!future) throw new NotFoundException('없는 미래입니다!');
-    Object.assign(future, attrs);
-    return this.futureRepo.save(future);
+    try {
+      Object.assign(future, attrs);
+      const newFuture = this.futureRepo.save(future);
+      await queryRunner.commitTransaction();
+      return newFuture;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e);
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async createFuture(attrs: FutureCreateDto) {
-    const future = this.futureRepo.create(attrs);
-    const futureBox = await this.futureBoxRepo.findOneBy({ id: attrs.boxId });
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const futureBox = await this.findOneFutureBox(attrs.boxId);
     if (!futureBox) throw new NotFoundException('없는 박스입니다!');
-    future.box = futureBox;
-    return this.futureRepo.save(future);
+    try {
+      const future = queryRunner.manager.getRepository('future').create(attrs);
+      future.future_box = futureBox;
+      let targetRepository: typeof FutureCheck | typeof FutureProgress;
+      if (futureBox.type === 'check') targetRepository = FutureCheck;
+      else if (futureBox.type === 'progress') targetRepository = FutureProgress;
+      const newFuture = await queryRunner.manager.getRepository(targetRepository).save(future);
+      await queryRunner.commitTransaction();
+      return newFuture;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e);
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async patchFutureBox(attrs: FutureBoxDto) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
     const futureBox = await this.futureBoxRepo.findOneBy({ id: attrs.id });
     if (!futureBox) throw new NotFoundException('없는 박스입니다!');
-    Object.assign(futureBox, attrs);
-    return this.futureBoxRepo.save(futureBox);
+    try {
+      Object.assign(futureBox, attrs);
+      const newFutureBox = this.futureBoxRepo.save(futureBox);
+      await queryRunner.commitTransaction();
+      return newFutureBox;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e);
+    } finally {
+      await queryRunner.release();
+    }
   }
 
   async createFutureBox(attrs: FutureBoxCreateDto) {
-    const futureBox = this.futureBoxRepo.create(attrs);
-    return this.futureBoxRepo.save(futureBox);
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const futureBox = queryRunner.manager.getRepository('future_box').create(attrs);
+      const newFutureBox = await queryRunner.manager.getRepository('future_box').save(futureBox);
+      await queryRunner.commitTransaction();
+      return newFutureBox;
+    } catch (e) {
+      await queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(e);
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
