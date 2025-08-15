5d8f5b2e2cbc66697280e82c2f3e7fc2fdd71fcc
kwondns <kwondns@gmail.com>
2025-08-15 18:40:23 +0900

    🚀 [Feat]: app.config.ts에 새로운 환경 변수 추가 및 구조 개선
    - `auth` 설정 활성화: `MAIL_USER`, `MAIL_PASSWORD` 재사용 가능
    - `db` 설정 추가
    - `jwt` 설정 구조 정리
    - `timeline`: `FRONT_URL`, `CHATBOT_URL`, `FILE_DESTROY_DELAY` 관리

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,23 +1,35 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
-  // auth: {
-  //   mailUser: process.env.MAIL_USER,
-  //   mailPassword: process.env.MAIL_PASSWORD,
-  // },
+  auth: {
+    mailUser: process.env.MAIL_USER,
+    mailPassword: process.env.MAIL_PASSWORD,
+  },
+  db: {
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    dbUserName: process.env.DB_USER_NAME,
+    dbHost: process.env.DB_HOST,
+    dbPassword: process.env.DB_PASSWORD,
+  },
   jwt: {
-    accessSecret: process.env.ACCESS_SECRET_KEY,
+    accessSecret: process.env.ACCESS_SECRET,
     accessExpire: process.env.ACCESS_EXPIRE,
-    refreshSecret: process.env.REFRESH_SECRET_KEY,
+    refreshSecret: process.env.REFRESH_SECRET,
     refreshExpire: process.env.REFRESH_EXPIRE,
-    // resetPasswordSecret: process.env.RESET_PASSWORD_SECRET_KEY,
-    // resetPasswordExpire: process.env.RESET_PASSWORD_EXPIRE,
+    resetPasswordSecret: process.env.RESET_PASSWORD_SECRET,
+    resetPasswordExpire: process.env.RESET_PASSWORD_EXPIRE,
   },
   s3: {
     s3Secret: process.env.S3_SECRET_KEY,
     s3Access: process.env.S3_ACCESS_KEY,
     s3Env: process.env.S3_ENV,
     s3TmpArchiveBucket: process.env.S3_TMP_ARCHIVE_BUCKET,
+  },
+  timeline: {
+    frontURL: process.env.FRONT_URL,
+    chatbotURL: process.env.CHATBOT_URL,
+    fileDestroyDelay: process.env.FILE_DESTROY_DELAY,
   },
   // oauth: {
   //   googleClientId: process.env.GOOGLE_CLIENT_ID,
@@ -31,6 +43,4 @@ export default registerAs('app', () => ({
   //   naverRedirectURI: process.env.NAVER_REDIRECT_URI,
   //   naverSecret: process.env.NAVER_CLIENT_SECRET,
   // },
-  // frontURL: process.env.FRONT_URL,
-  // fileDestroyDelay: process.env.FILE_DESTROY_DELAY,
 }));
