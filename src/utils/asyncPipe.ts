afbf4a69ad1276c9ba61164df3f57bac5fd32254
kwondns <kwondns@gmail.com>
2025-08-15 18:46:06 +0900

    ♻️ [Refactor]: asyncPipe 함수의 반환 타입 개선
    - `asyncPipe` 함수에 제네릭 타입 `R` 추가 및 반환 타입 명시
    - 함수의 결과를 명시적으로 `result as R`로 반환하도록 수정
    - 타입 안정성 및 코드 가독성 개선을 위한 리팩토링

--- src/utils/asyncPipe.ts
+++ src/utils/asyncPipe.ts
@@ -1,10 +1,10 @@
-const asyncPipe = async <T>(...fns: ((arg: T) => T | Promise<T>)[]) => {
+const asyncPipe = async <T, R = any>(...fns: ((arg: T) => T | Promise<T>)[]): Promise<(input: T) => Promise<R>> => {
   return async (input: any) => {
     let result = input;
     for (const fn of fns) {
       result = await fn(result); // 각 함수의 결과를 기다림
     }
-    return result;
+    return result as R;
   };
 };
 
