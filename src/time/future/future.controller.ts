4aef78f8d9a76f902563a750e022765e99cb1465
kwondns <kwondns@gmail.com>
2025-08-16 16:54:45 +0900

    🚀 [Feat]: Future 서비스에 User 연관 로직 추가
    - `getFutureBox`, `patchFuture`, `createFuture`,
    - `createFutureBox`, `getFutureAnalysis` 사용자 기준 필터링 추가
      - `User` 엔티티와 연관 관계를 통해 사용자 기반 데이터 조회 및 처리
      - `asyncPipe`를 활용하여 사용자 데이터 처리 로직 개선
    - `FutureDto`, `FuturePatchDto` 등 DTO 변경 및 데이터 구조 개선
      - 데이터 유효성 검사를 추가하고 필터링 기능 강화
    - `FutureBoxProgressView` 관련 로직 추가
      - `lastCompletedFuture` 정보를 포함한 데이터 반환
    - `FutureController` 메서드에 `CacheKeyHeaderInterceptor`와 `Serialize` 추가
      - 사용자 캐싱을 위한 인터셉터 설정 및 직렬화 로직 적용
    - 트랜잭션 로직 포함으로 데이터 무결성 보장
      - `future`, `futureBox` 생성 및 업데이트 시 오류 처리 강화

--- src/time/future/future.controller.ts
+++ src/time/future/future.controller.ts
@@ -1,43 +1,83 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, UseInterceptors } from '@nestjs/common';
 import { FutureService } from '@/time/future/future.service';
 import { FutureDto } from '@/time/dtos/future.dto';
 import { FutureBoxDto } from '@/time/dtos/futureBox.dto';
-import { Public } from '@/decorators/public.decorator';
 import { FutureCreateDto } from '@/time/dtos/futureCreate.dto';
 import { FutureBoxCreateDto } from '@/time/dtos/futureBoxCreate.dto';
+import { Serialize } from '@/interceptors/serialize.interceptor';
+import { ResponseFutureBoxDto } from '@/time/dtos/responseFuture.dto';
+import asyncPipe from '@/utils/asyncPipe';
+import { UserService } from '@/time/user/services/user.service';
+import { UserDto } from '@/time/dtos/user.dto';
+import { Future } from '@/time/entities/future.entity';
+import { FutureBox } from '@/time/entities/futureBox.entity';
+import { CacheKeyHeaderInterceptor } from '@/interceptors/cacheKeyHeader.interceptor';
 
+@Serialize(ResponseFutureBoxDto)
+@UseInterceptors(CacheKeyHeaderInterceptor)
 @Controller('/time/future')
 export class FutureController {
-  constructor(private futureService: FutureService) {}
+  constructor(
+    private readonly futureService: FutureService,
+    private readonly userService: UserService,
+  ) {}
 
-  @Public()
-  @Get(':priority')
-  getFutureBox(@Param('priority') priority: number) {
-    return this.futureService.getFutureBox(priority);
+  @Get()
+  async getFutureBox(@Body() body: UserDto) {
+    const pipeline = await asyncPipe(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.getFutureBox.bind(this.futureService),
+    );
+    return pipeline(body);
   }
-  @Public()
-  @Get(':priority/record')
-  getFutureBoxRecord(@Param('priority') priority: number) {
-    return this.futureService.getFutureBoxRecord(priority);
+
+  @Get('record')
+  getFutureBoxRecord() {
+    return this.futureService.getFutureBoxRecord();
   }
 
   @Patch()
-  patchFuture(@Body() body: FutureDto) {
-    return this.futureService.patchFuture(body);
+  async patchFuture(@Body() body: FutureDto) {
+    const pipeline = await asyncPipe(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.patchFuture.bind(this.futureService),
+    );
+    return await pipeline(body);
   }
 
   @Post()
-  createFuture(@Body() body: FutureCreateDto) {
-    return this.futureService.createFuture(body);
+  async createFuture(@Body() body: FutureCreateDto) {
+    const pipeline = await asyncPipe<FutureCreateDto, Future>(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.createFuture.bind(this.futureService),
+    );
+    return await pipeline(body);
   }
 
   @Post('box')
-  createFutureBox(@Body() body: FutureBoxCreateDto) {
-    return this.futureService.createFutureBox(body);
+  async createFutureBox(@Body() body: FutureBoxCreateDto) {
+    const pipeline = await asyncPipe<FutureBoxCreateDto, FutureBox>(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.createFutureBox.bind(this.futureService),
+    );
+    return await pipeline(body);
   }
 
   @Patch('box')
-  patchFutureBox(@Body() body: FutureBoxDto) {
-    return this.futureService.patchFutureBox(body);
+  async patchFutureBox(@Body() body: FutureBoxDto) {
+    const pipeline = await asyncPipe<FutureBoxDto, FutureBox>(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.patchFutureBox.bind(this.futureService),
+    );
+    return await pipeline(body);
+  }
+
+  @Get('analysis')
+  async getFutureAnalysis(@Body() body: UserDto) {
+    const pipeline = await asyncPipe(
+      this.userService.findUserByUserId.bind(this.userService),
+      this.futureService.getFutureAnalysis.bind(this.futureService),
+    );
+    return await pipeline(body);
   }
 }
--- src/time/future/future.service.ts
+++ src/time/future/future.service.ts
@@ -1,45 +1,67 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Future, FutureCheck, FutureProgress } from '@/time/entities/future.entity';
-import { DataSource, Repository } from 'typeorm';
-import { FutureBox } from '@/time/entities/futureBox.entity';
-import { FutureDto } from '@/time/dtos/future.dto';
+import { Brackets, DataSource, Repository } from 'typeorm';
+import { FutureBox, FutureBoxProgressView } from '@/time/entities/futureBox.entity';
 import { FutureBoxDto } from '@/time/dtos/futureBox.dto';
-import { FutureCreateDto } from '@/time/dtos/futureCreate.dto';
-import { FutureBoxCreateDto } from '@/time/dtos/futureBoxCreate.dto';
+import { FutureCreateServiceDto } from '@/time/dtos/futureCreate.dto';
+import { FutureBoxCreateServiceDto } from '@/time/dtos/futureBoxCreate.dto';
+import { User } from '@/time/user/entities/user.entity';
+import { FuturePatchDto } from '@/time/dtos/futurePatch.dto';
 
 @Injectable()
 export class FutureService {
   constructor(
     @InjectRepository(Future) private readonly futureRepo: Repository<Future>,
     @InjectRepository(FutureBox) private readonly futureBoxRepo: Repository<FutureBox>,
+    @InjectRepository(FutureBoxProgressView)
+    private readonly futureBoxProgressViewRepo: Repository<FutureBoxProgressView>,
     private readonly dataSource: DataSource,
   ) {}
 
-  async getFutureBox() {
-    return this.futureBoxRepo.find({
-      where: { checked: false },
-      order: { order: 'DESC' },
-      relations: ['future'],
-    });
+  async getFutureBox({ user }: { user: User }) {
+    const result = this.futureBoxRepo
+      .createQueryBuilder('future_box')
+      .leftJoinAndSelect('future_box.future', 'future')
+      .where('future_box.user = :userId', { userId: user.user_id })
+      .andWhere('future_box.checked = :checked', { checked: false })
+      .andWhere(
+        new Brackets((qb) => {
+          qb.where('future.type = :checkType AND future.checked = :futureChecked And future.user_id = :userId', {
+            checkType: 'FutureCheck',
+            futureChecked: false,
+            userId: user.user_id,
+          })
+            .orWhere('future.type = :progressType AND future.percentage < :maxPercentage', {
+              progressType: 'FutureProgress',
+              maxPercentage: 100,
+            })
+            .orWhere('future.id IS NULL');
+        }),
+      )
+      .orderBy('future_box.order', 'DESC')
+      .getMany();
+
+    return result;
   }
 
   async getFutureBoxRecord() {
     return this.futureBoxRepo.find({ relations: ['future'] });
   }
 
-  async findOneFutureBox(id: string) {
-    return this.futureBoxRepo.findOneBy({ id });
+  async findOneFutureBox(user_id: string, id: string) {
+    return this.futureBoxRepo.findOneBy({ id, user: { user_id: user_id } });
   }
 
-  async patchFuture(attrs: FutureDto) {
+  async patchFuture(dto: FuturePatchDto) {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
-    const future = await this.futureRepo.findOneBy({ id: attrs.id });
+    const { user, ...others } = dto;
+    const future = await this.futureRepo.findOneBy({ id: others.id, user: { user_id: user.user_id } });
     if (!future) throw new NotFoundException('없는 미래입니다!');
     try {
-      Object.assign(future, attrs);
+      Object.assign(future, others);
       const newFuture = this.futureRepo.save(future);
       await queryRunner.commitTransaction();
       return newFuture;
@@ -51,14 +73,14 @@ export class FutureService {
     }
   }
 
-  async createFuture(attrs: FutureCreateDto) {
+  async createFuture(dto: FutureCreateServiceDto) {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
-    const futureBox = await this.findOneFutureBox(attrs.boxId);
+    const futureBox = await this.findOneFutureBox(dto.user.user_id, dto.boxId);
     if (!futureBox) throw new NotFoundException('없는 박스입니다!');
     try {
-      const future = queryRunner.manager.getRepository('future').create(attrs);
+      const future = queryRunner.manager.getRepository('future').create(dto);
       future.future_box = futureBox;
       let targetRepository: typeof FutureCheck | typeof FutureProgress;
       if (futureBox.type === 'check') targetRepository = FutureCheck;
@@ -93,20 +115,45 @@ export class FutureService {
     }
   }
 
-  async createFutureBox(attrs: FutureBoxCreateDto) {
+  async createFutureBox(dto: FutureBoxCreateServiceDto) {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     try {
-      const futureBox = queryRunner.manager.getRepository('future_box').create(attrs);
+      const futureBox = queryRunner.manager.getRepository('future_box').create(dto);
+      console.log(futureBox);
       const newFutureBox = await queryRunner.manager.getRepository('future_box').save(futureBox);
       await queryRunner.commitTransaction();
       return newFutureBox;
     } catch (e) {
+      console.log(e);
       await queryRunner.rollbackTransaction();
       throw new InternalServerErrorException(e);
     } finally {
       await queryRunner.release();
     }
+  }
+
+  async getFutureAnalysis({ user }: { user: User }) {
+    const futureBoxes = await this.futureBoxProgressViewRepo.find({
+      where: { checked: false, user_id: user.user_id },
+      order: { updated_at: 'DESC' },
+    });
+
+    const getLastCompletedFuture = (futureBox: FutureBoxProgressView) => {
+      const statusWhere = futureBox.type === 'check' ? { checked: true } : { percentage: 100 };
+      return this.futureRepo.find({
+        where: { future_box: { id: futureBox.id }, user: { user_id: user.user_id }, ...statusWhere },
+        order: { updated_at: 'DESC' },
+        take: 3,
+      });
+    };
+
+    return await Promise.all(
+      futureBoxes.map(async (futureBox) => ({
+        ...futureBox,
+        lastCompletedFuture: await getLastCompletedFuture(futureBox),
+      })),
+    );
   }
 }
