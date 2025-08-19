efc795a7e4e1c5064294845bb3030d0a5b831ac7
kwondns <kwondns@gmail.com>
2025-08-19 19:55:07 +0900

    🚀 [Feat]: 회원 DTO 유효성 검사 메시지 추가
    - 사용자 유효성 검사 메시지 추가
      - 이메일 유효성 검사
      - 비밀번호 유효성 검사
      - 이름 유효성 검사

--- src/time/user/dto/sign-in.dto.ts
+++ src/time/user/dto/sign-in.dto.ts
@@ -3,10 +3,13 @@ import { Expose } from 'class-transformer';
 import { User } from '@/time/user/entities/user.entity';
 
 export class SignInDto {
-  @IsEmail()
+  @IsEmail({}, { message: '유효한 이메일 주소를 입력해주세요.' })
   email: string;
 
-  @IsStrongPassword({ minSymbols: 1, minLength: 8, minNumbers: 1, minLowercase: 0, minUppercase: 0 })
+  @IsStrongPassword(
+    { minSymbols: 1, minLength: 8, minNumbers: 1, minLowercase: 0, minUppercase: 0 },
+    { message: '비밀번호는 최소 8자리 이상이며, 숫자와 특수문자를 각각 1개 이상 포함해야 합니다.' },
+  )
   password: string;
 }
 
--- src/time/user/dto/update-user.dto.ts
+++ src/time/user/dto/update-user.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, IsJWT, IsString, IsStrongPassword, IsUUID } from 'class-valida
 import { Expose } from 'class-transformer';
 
 export class RequestResetPasswordDto {
-  @IsEmail()
+  @IsEmail({}, { message: '유효한 이메일 주소를 입력해주세요.' })
   email: string;
 }
 
@@ -15,7 +15,10 @@ export class VerifyResetPasswordDto {
 }
 
 export class UpdatePasswordDto extends VerifyResetPasswordDto {
-  @IsStrongPassword()
+  @IsStrongPassword(
+    { minSymbols: 1, minLength: 8, minNumbers: 1, minLowercase: 0, minUppercase: 0 },
+    { message: '비밀번호는 최소 8자리 이상이며, 숫자와 특수문자를 각각 1개 이상 포함해야 합니다.' },
+  )
   password: string;
 }
 
