23b0223ecd095e362d50cb6c30ba92cd418e4989
kwondns <kwondns@gmail.com>
2025-08-25 17:16:54 +0900

    🚑️ [Fix]: PastSubscriber 불필요한 데이터 할당 로직 제거
    - `event.queryRunner.data.entity` 값 할당 로직 제거
      - 사용되지 않는 데이터로 인한 불필요한 코드 최소화

--- src/time/subscribers/past.subscriber.ts
+++ src/time/subscribers/past.subscriber.ts
@@ -40,7 +40,6 @@ export class PastSubscriber implements EntitySubscriberInterface<Past> {
     }
     pastCount.count += diffMinute;
     await event.queryRunner.manager.save(PastCount, pastCount);
-    event.queryRunner.data.entity = 'past';
 
     const present = await event.queryRunner.manager.findOne(Present, {
       where: { user: { user_id: event.entity.user.user_id } },
