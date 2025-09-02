ef49ebd29373f94457e4d265fe1a8a298eb8bb56
kwondns <kwondns@gmail.com>
2025-09-02 16:22:35 +0900

    ♻️ [Refactor]: ecosystem.config 수정
    - 기존 env 설정을 제거
    - NODE_ENV만 설정
    - 개발 환경도 바로 적용할 수 있도록 추가
    - pm2 start ecosystem.config.js --env production
      - 프로덕션 환경
    - pm2 start ecosystem.config.js --env development_local
      - 개발 환경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,3 @@
-/* eslint-disable */
-const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
 module.exports = {
   apps: [
     {
@@ -10,37 +5,19 @@ module.exports = {
       script: './dist/main.js',
       instances: 'max',
       exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 5440,
-        DB_NAME: process.env.DB_NAME,
-        DB_PORT: process.env.DB_PORT,
-        DB_USERNAME: process.env.DB_USERNAME,
-        DB_HOST: process.env.DB_HOST,
-        DB_PASSWORD: process.env.DB_PASSWORD,
-        ACCESS_SECRET_KEY: process.env.ACCESS_SECRET_KEY,
-        ACCESS_EXPIRE: process.env.ACCESS_EXPIRE,
-        REFRESH_SECRET_KEY: process.env.REFRESH_SECRET_KEY,
-        REFRESH_EXPIRE: process.env.REFRESH_EXPIRE,
-        S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
-        S3_SECRET_KEY: process.env.S3_SECRET_KEY,
-        S3_ENV: process.env.S3_ENV,
-        ELASTICACHE_HOST: process.env.ELASTICACHE_HOST,
-        ELASTICACHE_PORT: process.env.ELASTICACHE_PORT,
-        DB_SSL_PATH: process.env.DB_SSL_PATH,
-        MAIL_USER: process.env.MAIL_USER,
-        MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-        RESET_PASSWORD_SECRET_KEY: process.env.RESET_PASSWORD_SECRET_KEY,
-        RESET_PASSWORD_EXPIRE: process.env.RESET_PASSWORD_EXPIRE,
-        FILE_DESTROY_DELAY: process.env.FILE_DESTROY_DELAY,
-        FRONT_URL: process.env.FRONT_URL,
-        CHATBOT_URL: process.env.CHATBOT_URL,
-        USER_ID: process.env.USER_ID,
-        DEMO_USER_ID: process.env.DEMO_USER_ID,
+
+      env_development_local: {
+        NODE_ENV: 'development',
+        env_file: '.env.development.local',
       },
-      log_date_format: 'YYYY-MM-DD HH:mm Z',
-      error_file: '/home/ec2-user/be/log/pm2/error.log',
-      out_file: '/home/ec2-user/be/log/pm2/out.log',
+      env_production: {
+        NODE_ENV: 'production',
+        env_file: '.env',
+      },
+
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      error_file: './logs/pm2/error.log',
+      out_file: './logs/pm2/out.log',
     },
   ],
 };
