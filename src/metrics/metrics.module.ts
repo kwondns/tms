f2b9c7dbbe70d29e0e85862a1b0f679d5b3e093f
kwondns <kwondns@gmail.com>
2025-09-04 16:48:25 +0900

    ♻️ [Refactor]: Metrics 모듈 수정
    - 동일한 register에 데이터가 쌓이도록 지정
    - 동일한 코드를 provider, export 되도록 분리

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -1,43 +1,32 @@
 import { Module } from '@nestjs/common';
 import { makeCounterProvider, makeHistogramProvider, PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { register } from 'prom-client';
+
+const metricsProviders = [
+  makeCounterProvider({
+    name: 'http_requests_total',
+    help: '전체 http 요청 수',
+    labelNames: ['method', 'route'],
+    registers: [register],
+  }),
+  makeCounterProvider({
+    name: 'http_response_status',
+    help: '상태 코드별 HTTP 응답 수',
+    labelNames: ['method', 'status'],
+    registers: [register],
+  }),
+  makeHistogramProvider({
+    name: 'http_request_duration_seconds',
+    help: 'HTTP 요청 처리 시간(초)',
+    labelNames: ['method', 'route', 'status'],
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5],
+    registers: [register],
+  }),
+];
 
 @Module({
   imports: [PrometheusModule.register()],
-  providers: [
-    makeCounterProvider({
-      name: 'http_requests_total',
-      help: '전체 http 요청 수',
-      labelNames: ['method', 'route'],
-    }),
-    makeCounterProvider({
-      name: 'http_response_status',
-      help: '상태 코드별 HTTP 응답 수',
-      labelNames: ['method', 'status', 'http'],
-    }),
-    makeHistogramProvider({
-      name: 'http_request_duration_seconds',
-      help: 'HTTP 요청 처리 시간(초)',
-      labelNames: ['method', 'route', 'status'],
-      buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5], // 실무에 적합한 버킷 설정
-    }),
-  ],
-  exports: [
-    makeCounterProvider({
-      name: 'http_requests_total',
-      help: '전체 http 요청 수',
-      labelNames: ['method', 'route'],
-    }),
-    makeCounterProvider({
-      name: 'http_response_status',
-      help: '상태 코드별 HTTP 응답 수',
-      labelNames: ['method', 'status', 'http'],
-    }),
-    makeHistogramProvider({
-      name: 'http_request_duration_seconds',
-      help: 'HTTP 요청 처리 시간(초)',
-      labelNames: ['method', 'route', 'status'],
-      buckets: [0.05, 0.1, 0.2, 0.5, 1, 2, 5], // 실무에 적합한 버킷 설정
-    }),
-  ],
+  providers: [...metricsProviders],
+  exports: [...metricsProviders],
 })
 export class MetricsModule {}
